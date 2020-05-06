51561e23c24513a3b0499b2e73d15d39810d9701
devkyeong <devkyeong@gmail.com>
2020-05-06 17:13:53 +0900

    Initial commit

---
+++ src/main/java/me/kyeong/webservice/WebRestController.java
@@ -0,0 +1,13 @@
+package me.kyeong.webservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "helloWorld";
+    }
+}
